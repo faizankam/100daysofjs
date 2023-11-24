658a6f43e954b9a1f54697e33f2113f9c310c482
Faizan Kamboh <kamboh8208@gmail.com>
2023-11-24 15:25:37 +0500

    Add files via upload

---
+++ 2 day/script.js
@@ -0,0 +1,55 @@
+var x = "Faizan" + "kamboh";
+console.log(x);
+let a = `Faizan  Kamboh`;
+console.log(a);
+let b = "Faizan" + " " + "kambooh";
+console.log(b);
+var f = 7 ** 2;
+console.log(f);
+var d = 9984 % 9;
+console.log(d);
+var ds = 5;
+let ad = (100 + 50) * ds;
+console.log(ad);
+let sd = 5;
+sd++;
+let fff = sd;
+console.log(fff);
+let dsd = 10;
+var dsds;
+dsds = 252;
+dsd &= dsds;
+console.log(dsd);
+let kam = -100;
+kam <<= 5;
+console.log(kam); // In JavaScript, the code x <<= 5; is equivalent to multiplying the variable x by 2 raised to the power of 5 and then assigning the result back to x. It's a bitwise left shift operation, which effectively shifts the bits of the binary representation of the number to the left.
+
+
+// new code without console.log  of Datatypes
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let xa = true;
+let ysd = false;
+
+// Object:
+const person = { firstName: "John", lastName: "Doe" };
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+
+//let fun
+const fai = {
+    firstName: "John",
+    lastName: "Doe"
+};   
